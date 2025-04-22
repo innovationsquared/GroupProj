470c8107e8d41cd655d945ef1e2ce96484bdd8b1
innovationsquared <pettiscd@appstate.edu>
2025-04-22 01:41:51 -0400

    Basic server

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # GroupProj
+**Data Visualization surrounding data scraped from GitHub**  
+
+### Steps to Install
+
+1. **Clone the repository**
+   ```bash
+   git clone https://github.com/innovationsquared/GroupProj.git   
+   ```
+2. **Run the local server**
+    ```bash
+    npm start
+    ```
---
+++ index.js
@@ -0,0 +1,30 @@
+//This is a simple backend server.
+const express = require('express');
+const app = express();
+const PORT = 8000;
+const data = require('./data/repository_data.csv');
+console.log(data);
+
+app.use(express.static('public'));
+//app.use('/tree.js', express.static('tree.js'));
+
+app.get('/start', (req, res)=>{
+  res.send(Object.keys(data));
+});
+
+app.listen(PORT, ()=> {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+app.get('/',(req,res)=> {
+res.send("Hello, Server!");
+});
+
+app.get('/data', (req, res)=> {
+  //const name = data[req.query.k];
+  //const tree = data[name];
+  const key = req.query.key
+  res.send(data[key]);
+});
+
+//app.post
